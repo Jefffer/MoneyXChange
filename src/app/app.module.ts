79af14933361dc87876c29799c5c42926c830939
Jefffer <jefre123@hotmail.com>
2019-12-26 12:19:46 -0500

    Conversions and conversions/conversion components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConversionsComponent } from './conversions/conversions.component';
+import { ConversionComponent } from './conversions/conversion/conversion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversionsComponent,
+    ConversionComponent
   ],
   imports: [
     BrowserModule
